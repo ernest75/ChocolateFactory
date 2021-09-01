ee6611d5f09f470aae3472606641d65e8d74bd93
ernest75 <ernestschneider@gmail.com>
2021-09-01 19:47:42 +0200

    Added Coroutines dependency + scope implementation

--- app/src/main/java/com/example/chocolatefactory/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/chocolatefactory/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.chocolatefactory
+package com.example.chocolatefactory.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.chocolatefactory.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/chocolatefactory/ui/main/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.chocolatefactory.ui.main
+
+import com.example.chocolatefactory.common.ScopedViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+
+class MainViewModel(uiDispatcher: CoroutineDispatcher)
+    : ScopedViewModel(uiDispatcher)  {
+
+}
